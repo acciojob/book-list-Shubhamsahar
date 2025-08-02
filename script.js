1a66301158172791ec0ab2484ae0331a26702d2d
Shubhamsahar <kumarshubham765484@gmail.com>
2025-08-02 10:15:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,33 +1,39 @@
-//your JS code here. If required.
+document.getElementById('book-form').addEventListener('submit', addBook);
 
-let bookForm = document.getElementById('book-form');
-let bookList = document.getElementById('book-list');
-
-bookForm.addEventListener('submit', (e) => {
+function addBook(e) {
     e.preventDefault();
     let title = document.getElementById('title').value;
     let author = document.getElementById('author').value;
     let isbn = document.getElementById('isbn').value;
 
-    if (title && author && isbn) {
-        let row = document.createElement('tr');
-        row.innerHTML = `
-            <td>${title}</td>
-            <td>${author}</td>
-            <td>${isbn}</td>
-            <td><span class="delete">Clear</span></td>
-        `;
-        bookList.appendChild(row);
-
-        document.getElementById('title').value = '';
-        document.getElementById('author').value = '';
-        document.getElementById('isbn').value = '';
-
-        let deleteSpans = document.getElementsByClassName('delete');
-        for (let i = 0; i < deleteSpans.length; i++) {
-            deleteSpans[i].addEventListener('click', () => {
-                deleteSpans[i].parentNode.parentNode.remove();
-            });
-        }
+    if (title === '' || author === '' || isbn === '') {
+        alert('Please fill in all fields');
+        return;
     }
-});
+
+    let bookList = document.getElementById('book-list');
+    let row = document.createElement('tr');
+    row.innerHTML = `
+        <td>${title}</td>
+        <td>${author}</td>
+        <td>${isbn}</td>
+        <td><a class="delete" href="#">Clear</a></td>
+    `;
+    bookList.appendChild(row);
+
+    document.getElementById('title').value = '';
+    document.getElementById('author').value = '';
+    document.getElementById('isbn').value = '';
+
+    let deleteLinks = document.getElementsByClassName('delete');
+    for (let i = 0; i < deleteLinks.length; i++) {
+        deleteLinks[i].addEventListener('click', deleteBook);
+    }
+}
+
+function deleteBook(e) {
+    e.preventDefault();
+    if (confirm('Are you sure you want to delete this book?')) {
+        e.target.parentNode.parentNode.remove();
+    }
+}
